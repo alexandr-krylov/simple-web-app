8e60e9ab9e30290e93ea472be2c0c414a4628ce3
Alexandr Krylov <alexandr.krylov@gmail.com>
2020-02-19 08:47:22 +0600

    migration create tables

---
+++ application/laravel/database/migrations/2020_02_19_022130_create_employees-departments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees-departments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('employee_id');
+            $table->unsignedBigInteger('department_id');
+            $table->index(['employee_id', 'department_id']);
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees-departments');
+    }
+}
