e65ffbf917470c2bc03554564f393d739a6f8562
Alexandr Krylov <alexandr.krylov@gmail.com>
2020-02-25 14:32:04 +0600

    departments

--- application/laravel/app/Http/Controllers/DepartmentController.php
+++ application/laravel/app/Http/Controllers/DepartmentController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Core\Repository\DepartmentRepositoryInterface;
+
 class DepartmentController extends Controller
 {
+    private $repository;
+
+    public function __construct(DepartmentRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function show()
     {
+        $departments = $repository->all();
+        if (empty($departments)) {
+            $data['message'] = ['type' => 'info', 'content' => 'Departments is empty'];
+        }
         $data['activeDepartment'] = 'active';
         return view('department', $data);
     }
